2262634e7f4b56c711c5a5ae753addf0bc7921bd
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-13 08:32:51 +1000

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,67 +8,43 @@
  * @second: second elem
 */
 
-void selection_sort(int *array, size_t size)
-{
-  size_t min, i, j;
-  int temp;
-
-  for (i = 0; i < size; i++)
-    {
-      min = i;
-      
-      for (j = i + 1; j < size; j++)
-        {
-          if (array[j] < array[min])
-            min = j;
-        }
-      temp = array[i];
-      array[i] = array[min];
-      array[min] = temp;
-      
-      print_array(array, size);
-    }
-}
-
-/**
 void swap(int *first, int *second)
 {
-  int temp = *first;
-  *first = *second;
-  *second = temp;
+    int temp = *first;
+    *first = *second;
+    *second = temp;
 }
-*/
 
 /**
  * selection_sort - sort array of ints in ascending order
  * @array: array containing all elements
  * @size: number of elements in array
 */
-/**
+
 void selection_sort(int *array, size_t size)
 {
-  size_t i, min_element, j;
-  i = 0;
-  if (array == NULL || size < 2)
-  {
-    return;
-  }
-  while(i < (size - 1))
-  {
-    min_element = i;
-    j = i + 1;
-    while (j < size)
+    size_t i, min_element, j;
+    i = 0;
+    if (array == NULL || size < 2)
     {
-      if (array[j] < array[min_element])
-        min_element = j;
-      j++;
+      return;
     }
-    if (min_element != i)
+    while(i < (size - 1))
     {
-      swap(&array[min_element], &array[i]);
-      print_array(array, size);
+      min_element = i;
+      j = i + 1;
+      while (j < size)
+      {
+        if (array[j] < array[min_element])
+          min_element = j;
+        j++;
+      }
+      if (min_element != i)
+      {
+        swap(&array[min_element], &array[i]);
+        print_array(array, size);
+      }
+      i++;
     }
-    i++;
-  }
 }
-*/
+
