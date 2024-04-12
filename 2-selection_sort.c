f5151f07502a931c54b15a9597014712a762f0c7
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-12 21:45:25 +1000

    test run 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,22 +1,59 @@
 #include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * selection_sort - sort array of ints in ascending order
+ * @array: array containing all elements
+ * @size: number of elements in array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+  size_t min, i, j;
+  int temp;
+
+  for (i = 0; i < size; i++)
+    {
+      min = i;
+      
+      for (j = i + 1; j < size; j++)
+        {
+          if (array[j] < array[min])
+          {
+            min = j;
+          }
+        }
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+      
+      print_array(array, size);
+    }
+}
+
 /**
  * swap - swap first element with second elem
  * @first: first elem
  * @second: second elem
 */
+
+/**
 void swap(int *first, int *second)
 {
 int temp = *first;
 *first = *second;
 *second = temp;
 }
+*/
+
 /**
  * selection_sort - sort array of ints in ascending order
  * @array: array containing all elements
  * @size: number of elements in array
 */
+
+/**
 void selection_sort(int *array, size_t size)
 {
 size_t i, min_element, j;
@@ -43,3 +80,4 @@ print_array(array, size);
 i++;
 }
 }
+*/
