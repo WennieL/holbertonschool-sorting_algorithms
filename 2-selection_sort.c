b9a0454df723afcb2d486d20f0e73af633c1ac3e
WennieL <jecxu@hotmail.com>
2024-04-12 22:59:51 +0000

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,49 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * swap - swap first element with second elem
- * @first: first elem
- * @second: second elem
-*/
-
-void swap(int *first, int *second)
-{
-    int temp = *first;
-    *first = *second;
-    *second = temp;
-}
-
-/**
- * selection_sort - sort array of ints in ascending order
- * @array: array containing all elements
- * @size: number of elements in array
-*/
-
 void selection_sort(int *array, size_t size)
 {
-    size_t i, min_element, j;
-    i = 0;
-    if (array == NULL || size < 2)
-    {
-      return;
-    }
-    while(i < (size - 1))
-    {
-      min_element = i;
-      j = i + 1;
-      while (j < size)
-      {
-        if (array[j] < array[min_element])
-          min_element = j;
-        j++;
-      }
-      if (min_element != i)
-      {
-        swap(&array[min_element], &array[i]);
-        print_array(array, size);
-      }
-      i++;
-    }
-}
+	size_t i, j, min;
+	int temp;
 
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+
+		print_array(array, size);
+	}
+}
