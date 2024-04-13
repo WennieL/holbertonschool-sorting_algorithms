17f3ee02d775e45a02015dd9970db74b2df7d55d
WennieL <jecxu@hotmail.com>
2024-04-13 04:03:50 +0000

    Task 3

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(int *first, int *second);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 #endif /*SORT_H*/
